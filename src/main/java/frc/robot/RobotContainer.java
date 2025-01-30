57aeb7ad97491174d818317af6efad1175512b42
glerpstudios <120259946+glerpstudios@users.noreply.github.com>
2025-01-29 21:05:43 -0500

    Finalized design 1 + JS_IO-like implementation + hdw

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@
 package frc.robot;
 
 // Constants
+import frc.robot.Constants.OperatorConstants;
 
 // Operator Input
 import frc.robot.OperatorInput;
@@ -30,6 +31,12 @@ import frc.robot.OperatorInput;
 import frc.robot.commandgroups.Level2CommandGroup;
 import frc.robot.commandgroups.Level3CommandGroup;
 import frc.robot.commandgroups.Level4CommandGroup;
+
+// Elevator Commands
+// NONE YET!
+
+// Arm Commands
+// NONE YET!
 
 // Interface Commands
 import frc.robot.commands.InterfaceStoreBranchesCommand;
@@ -57,45 +64,26 @@ import frc.robot.subsystems.InterfaceSubsystem;
 //   `Y8bood8P'  o888o 
 //                    
 
+// Operator Input Imports
+// ----------------------Logitech Extreme 3D Pro
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+// ----------------------Xbox Controller
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 // Necessary stuff
 // import edu.wpi.first.wpilibj2.command.Command;  <------------- Uncomment this if you are using a command without a group
-=======
-import frc.robot.Constants.OperatorConstants;
-
-import frc.robot.subsystems.ElevatorSubsys;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.ArmSubsys;
-import frc.robot.subsystems.ChangeBooleanSubsystem;
-// import frc.robot.subsystems.FlipSubsys;
-import frc.robot.subsystems.GripSubsys;
-import frc.robot.commands.elvatorCmds.ElevExecCmd;
-import frc.robot.commands.elvatorCmds.ElevSetRqCmd;
-import frc.robot.commands.elvatorCmds.ArmToggleCmd;
-import frc.robot.commands.elvatorCmds.GripToggleCmd;
-
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.ChangeBooleanCommand;
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final OperatorInput m_operatorInput = new OperatorInput(0);
+  private final OperatorInput m_operatorInput = new OperatorInput(1);
 
   private final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
   private final ArmSubsystem m_armSubsystem = new ArmSubsystem();
@@ -114,6 +102,15 @@ public class RobotContainer {
     configureBindings();
   }
 
+  //Logitech Extreme 3D Pro
+  public static Joystick driverJoystick = new Joystick(OperatorConstants.kDriverControllerPort);
+  public static JoystickButton lvl2Button = new JoystickButton(driverJoystick, 3);
+  public static JoystickButton lvl3Button = new JoystickButton(driverJoystick, 4);
+  public static JoystickButton lvl4Button = new JoystickButton(driverJoystick, 6);
+  public static JoystickButton selectBranchAndAddButton = new JoystickButton(driverJoystick, 1);
+
+  //Test: Keyboard
+
   
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
@@ -126,10 +123,10 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Logitech Extreme 3D Pro
-    m_operatorInput.lvl2Button.onTrue(m_level2CommandGroup);
-    m_operatorInput.lvl3Button.onTrue(m_level3CommandGroup);
-    m_operatorInput.lvl4Button.onTrue(m_level4CommandGroup);
-    m_operatorInput.selectBranchAndAddButton.onTrue(m_interfaceStoreBranchesCommand);
+    lvl2Button.onTrue(m_level2CommandGroup);
+    lvl3Button.onTrue(m_level3CommandGroup);
+    lvl4Button.onTrue(m_level4CommandGroup);
+    selectBranchAndAddButton.onTrue(m_interfaceStoreBranchesCommand);
 
     // Test: Keyboard
 
@@ -142,100 +139,5 @@ public class RobotContainer {
   //public Command getChangeBooleanCommand() {
     // An example command will be run in autonomous
   //  return new ChangeBooleanCommand(m_changeBooleanSubsystem);
-  //
-    // The robot's subsystems and commands are defined here...
-    private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-    private final ChangeBooleanSubsystem m_changeBooleanSubsystem = new ChangeBooleanSubsystem();
-    private final ChangeBooleanCommand m_changeBooleanCommand = new ChangeBooleanCommand(m_changeBooleanSubsystem);
-
-    private final ElevatorSubsys m_elevSubsys = new ElevatorSubsys();
-    private final GripSubsys m_gripSubsys = new GripSubsys();
-    private final ArmSubsys m_armSubsys = new ArmSubsys();
-    // private final FlipSubsys m_flipSubsys = new FlipSubsys();
-
-    private static final JS_OI jsbtn = new JS_OI(2); // 0=Normal 3 JS, 1=Neopad, 2=Keyboard
-
-    // The robot's subsystems and commands are defined here...
-
-    // Replace with CommandPS4Controller or CommandJoystick if needed
-    private final CommandXboxController m_driverController = new CommandXboxController(
-            OperatorConstants.kDriverControllerPort);
-
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-
-        configButtonBindings();
-
-        // Configure the trigger bindings
-        configureBindings();
-    }
-
-    public static Joystick driverJoystick = new Joystick(0);
-    public static JoystickButton changeBooleanButton = new JoystickButton(driverJoystick, 7);
-
-    public void configButtonBindings() {
-        // Need to select the level then execute when ready(?).
-        jsbtn.elevExecBtn.onTrue(new ElevExecCmd(m_elevSubsys));
-        jsbtn.elevLowBtn.onTrue(new ElevSetRqCmd(m_elevSubsys, 0));
-        jsbtn.elevMidBtn.onTrue(new ElevSetRqCmd(m_elevSubsys, 3));
-        jsbtn.elevHighBtn.onTrue(new ElevSetRqCmd(m_elevSubsys, 4));
-        // for arm toggle
-        jsbtn.armToggleBtn.onTrue(new ArmToggleCmd(m_armSubsys));
-        // for gripper
-        jsbtn.gripToggleBtn.onTrue(new GripToggleCmd(m_gripSubsys));
-    }
-
-    public void update() {
-        if (jsbtn.ChooserUpd())
-            configButtonBindings();
-    }
-
-    /**
-     * Use this method to define your trigger->command mappings. Triggers can be
-     * created via the
-     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-     * an arbitrary
-     * predicate, or via the named factories in {@link
-     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-     * {@link
-     * CommandXboxController
-     * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-     * PS4} controllers or
-     * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-     * joysticks}.
-     */
-    private void configureBindings() {
-        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-        // new Trigger(m_exampleSubsystem::exampleCondition)
-        // .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-        // Schedule `exampleMethodCommand` when the Xbox controller's B button is
-        // pressed,
-        // cancelling on release.
-        // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-
-        // changeBooleanButton.onTrue(m_changeBooleanCommand);
-    }
-
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // An example command will be run in autonomous
-        return Autos.exampleAuto(m_exampleSubsystem);
-    }
-
-    /**
-     * Use this to pass the boolean changer command to the main {@link Robot} class.
-     *
-     * @return the command to run in teleopPeriodic
-     */
-    // public Command getChangeBooleanCommand() {
-    // // An example command will be run in autonomous
-    // return new ChangeBooleanCommand(m_changeBooleanSubsystem);
-    // }
+  //}
 }
