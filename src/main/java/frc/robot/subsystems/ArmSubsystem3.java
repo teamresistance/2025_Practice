f1bdc3cd7e92eca905a461816e5417138a0f8f36
darkpotatoo <darkpotatoben@gmail.com>
2025-02-02 14:13:34 -0500

    gripper

--- src/main/java/frc/robot/subsystems/ArmSubsystem3.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem3.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -11,16 +12,32 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem3 extends SubsystemBase {
   private Solenoid arm = new Solenoid(PneumaticsModuleType.CTREPCM, 2);
+  private Solenoid gripper = new Solenoid(PneumaticsModuleType.CTREPCM, 3);
+  private DigitalInput gripperSensor = new DigitalInput(0);
+  private boolean cancelSwitch;
 
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem3() {}
 
     /** Command to toggle the elevator's rotating coral holder */
   public void ElevatorToggle(){
-    arm.set(!arm.get()); //When solenoids are added change this to a .toggle()
+    arm.set(!arm.get());
+    gripper.set(!gripper.get());
+    cancelSwitch=true;
   }
+
+  public void GripperToggle(){
+  }
+
   @Override
   public void periodic() {
+    if (gripperSensor.get()){
+      while (!cancelSwitch){
+        gripper.set(true);
+      }
+      gripper.set(false);
+      cancelSwitch=false;
+    }
     SmartDashboard.putBoolean("Arm Holder", arm.get());
     // This method will be called once per scheduler run
   }
