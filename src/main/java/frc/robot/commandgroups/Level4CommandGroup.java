bc6750fc47d59c5499ecf702723fbcd4657fa3f5
glerpstudios <120259946+glerpstudios@users.noreply.github.com>
2025-02-05 20:32:14 -0500

    Some files were not  saved.

--- src/main/java/frc/robot/commandgroups/Level4CommandGroup.java
+++ src/main/java/frc/robot/commandgroups/Level4CommandGroup.java
@@ -1,29 +1,47 @@
+
 package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 // Commands
-import frc.robot.commands.ElevatorRaiseFirstStageCommand;
-import frc.robot.commands.ElevatorLowerFirstStageCommand;
-import frc.robot.commands.ArmGetIntoScoringPositionAndScoreCommand;
-import frc.robot.commands.ArmRetractCommand;
+import frc.robot.commands.FlipperExtendAndScoreCommand;
+import frc.robot.commands.FlipperRetractCommand;
 import frc.robot.commands.ElevatorRaiseSecondStageCommand;
 import frc.robot.commands.ElevatorLowerSecondStageCommand;
-
+import frc.robot.commands.ElevatorRaiseFirstStageCommand;
+import frc.robot.commands.ElevatorLowerFirstStageCommand;
 // Subsystems
 import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.FlipperSubsystem;
 
 // Create a sequential command group for Level 4
 public class Level4CommandGroup extends SequentialCommandGroup {
-    public Level4CommandGroup(ElevatorSubsystem elevatorSubsystem, ArmSubsystem armSubsystem) {
+
+    public class RaiseBothStagesCommandGroup extends ParallelCommandGroup {
+        public RaiseBothStagesCommandGroup(ElevatorSubsystem elevatorSubsystem) {
+            addCommands(
+                new ElevatorRaiseFirstStageCommand(elevatorSubsystem),
+                new ElevatorRaiseSecondStageCommand(elevatorSubsystem)
+            );
+        }
+    }
+
+    public class LowerBothStagesCommandGroup extends ParallelCommandGroup {
+        public LowerBothStagesCommandGroup(ElevatorSubsystem elevatorSubsystem) {
+            addCommands(
+                new ElevatorLowerSecondStageCommand(elevatorSubsystem),
+                new ElevatorLowerFirstStageCommand(elevatorSubsystem)
+            );
+        }
+    }   
+
+    public Level4CommandGroup(ElevatorSubsystem elevatorSubsystem, FlipperSubsystem flipperSubsystem) {
         addCommands(
-            new ElevatorRaiseFirstStageCommand(elevatorSubsystem),
-            new ElevatorRaiseSecondStageCommand(elevatorSubsystem),
-            new ArmGetIntoScoringPositionAndScoreCommand(armSubsystem),
-            new ArmRetractCommand(armSubsystem),
-            new ElevatorLowerSecondStageCommand(elevatorSubsystem),
-            new ElevatorLowerFirstStageCommand(elevatorSubsystem)
+            new RaiseBothStagesCommandGroup(elevatorSubsystem),
+            new FlipperExtendAndScoreCommand(flipperSubsystem),
+            new FlipperRetractCommand(flipperSubsystem),
+            new LowerBothStagesCommandGroup(elevatorSubsystem)
         );
     }
 }
