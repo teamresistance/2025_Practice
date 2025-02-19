b50d703944c643713ad9b9e06e85a739810501e7
glerpstudios <120259946+glerpstudios@users.noreply.github.com>
2025-02-19 07:47:05 -0500

    Comment ElevatorSubsystem.java

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -3,19 +3,26 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
+// Command based imports
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.smartdashboard.*;
 
+// Logging imports
+import edu.wpi.first.wpilibj.smartdashboard.*;
 import org.littletonrobotics.junction.Logger;
 
+// Hardware Imports
 import frc.robot.Constants.HardwareConstants;
-
 import com.fasterxml.jackson.core.base.GeneratorBase;
-
 import edu.wpi.first.wpilibj.Solenoid;
 
 public class ElevatorSubsystem extends SubsystemBase {
+  //Variables
+
+  /*
+  * This solenoid  controls the first (lower) stage of the elevator.
+  * It must be set to true before {@code secondStageSolenoid} is.
+  * If it is on, L2 scoring is enabled.
+  */
   public Solenoid firstStageSolenoid = new Solenoid(
       HardwareConstants.pneumaticsModuleType,
       HardwareConstants.kSolenoid_firstStage_portNumber);
